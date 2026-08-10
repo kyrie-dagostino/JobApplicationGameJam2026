0ce9104ea3584f93c2d2bb2a31f2664b8538e274
kyrie-edelweiss <kyrie.edelweiss@gmail.com>
2026-08-11 02:58:01 +0300

    Family states for future points system

--- job-application-game/Assets/Scripts/family.gd
+++ job-application-game/Assets/Scripts/family.gd
@@ -4,7 +4,7 @@ signal scene_changed
 
 @onready var enter_timer: Timer = %enterTimer
 @onready var exit_timer: Timer = %exitTimer
-
+@export var family_state = false
 
 func _ready() -> void:
 	hide()
@@ -13,10 +13,12 @@ func _ready() -> void:
 func _on_enter_timer_timeout():
 	exit_timer.start()
 	scene_changed.emit()
+	family_state = true
 
 func _on_exit_timer_timeout():
 	enter_timer.start()
 	scene_changed.emit()
+	family_state = false
 
 func _on_fade_fade_ready() -> void:
 	visible = not visible
